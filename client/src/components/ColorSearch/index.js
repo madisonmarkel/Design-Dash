48dea3b754d340b78d92d714016289330cf5f500
Madison Markel <madisonmarkel@gmail.com>
2019-01-04 18:24:43 -0700

    thank you chad

--- client/src/components/ColorSearch/index.js
+++ client/src/components/ColorSearch/index.js
@@ -8,41 +8,22 @@ class ColorMindSearch extends Component {
 
     // componentDidUpdate () {
     callAPI = () => {
-        postData(`http://colormind.io/api/`)
-            .then(res => {
-                console.log(res); 
-                this.setState({ color: res }, () => {
-                    // this.props.getCalendarData(this.state.color)
-                    console.log(this.state.color);
-                    // this.state.color.result.map(colors => (
-                    //     <div //style={{ background: this.state.colors, padding: 10 }}
-                    //     >
-                    //       <p id={colors.result}>Color</p>
-                    //     </div>
-                    // ));
-                  });
-                console.log(this);
-                console.log(this.state);
-            })
-            .catch(error => console.error(error));
-
-            function postData(url = ``, data = {model : "default"}) 
-                {
-                // Default options are marked with *
-                return fetch(url, {
-                    method: "POST", // *GET, POST, PUT, DELETE, etc.
-                    mode: "cors", // no-cors, cors, *same-origin
-                    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-                    credentials: "same-origin", // include, *same-origin, omit
-                    redirect: "follow", // manual, *follow, error
-                    referrer: "no-referrer", // no-referrer, *client
-                    body: JSON.stringify(data), // body data type must match "Content-Type" header
-                })
-                .then(response => response.json()); // parses response to JSON
-            }
-        console.log(this.state);
+        return fetch(`http://colormind.io/api/`, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            redirect: "follow", // manual, *follow, error
+            referrer: "no-referrer", // no-referrer, *client
+            body: JSON.stringify({model : "default"}) // body data type must match "Content-Type" header
+        })
+        .then(response => response.json())
     }
 
+    setColor = () => {
+        this.callAPI().then(data => this.setState({color: data.result}))
+
+    }
       render() {
         //const { color } = this.state;
         return(
@@ -55,20 +36,20 @@ class ColorMindSearch extends Component {
                     id="pixabay_submit"
                     className="btn"
                     //onClick={this.handleClick}>
-                    onClick={this.callAPI}>
+                    onClick={this.setColor}>
                       Generate Colors
                   </button>
               </div>
               <div className="all_pixabay_results">
                 
-                    {/* {this.state.color.result.map(colors => (
+                    {this.state.color.map(colors => (
                         <div //style={{ background: this.state.colors, padding: 10 }}
                         >
                           <p id={colors.result}>Color</p>
                         </div>
-                    ))} */}
-                    {/* {this.state.map(colors => (
-                        <div //style={{ background: this.state.colors, padding: 10 }}
+                    ))}
+                    {/* {this.state.map(colors => ( */}
+                        {/* <div //style={{ background: this.state.colors, padding: 10 }}
                         >
                           <p id={colors}>Color</p>
                         </div>
