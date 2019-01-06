e406910c4e1268de743a559d306d05e52e3da31c
Madison Markel <madisonmarkel@gmail.com>
2019-01-06 14:46:44 -0700

    able to edit text fields in update now

--- client/src/components/UpdateBrandsModalInfo/index.js
+++ client/src/components/UpdateBrandsModalInfo/index.js
@@ -5,26 +5,29 @@ import { TextArea } from "../Form";
 
 class Brands extends Component {
   state = {
-    brands: [],
-    _id: "",
-    name: "",
-    industry: "",
-    slogan: "",
-    logo: "", 
-    mainColor: "",
-    supportingColor: "",
-    images: "",
+    brand: [],
+    // name: this.props.name,
+    // industry: this.props.industry,
+    // slogan: this.props.slogan,
+    // logo: this.props.logo, 
+    // mainColor: this.props.mainColor,
+    // supportingColor: this.props.supportingColor,
+    // images: this.props.images,
+    // brands: [],
+    // _id: "",
+    // name: "",
+    // industry: "",
+    // slogan: "",
+    // logo: "", 
+    // mainColor: "",
+    // supportingColor: "",
+    // images: "",
   };
-
-//   componentDidMount() {
-//     //API.getBrand(this.props.match.params.id)
-//     API.getBrand(this.state.brand.id)
-//       .then(res => this.setState({ brand: res.data }))
-//       .catch(err => console.log(err));
-//     console.log(this);
-//     console.log(this.state.brand.id);
-//     console.log(this.state._id);
-//   }
+  componentDidMount() {
+    API.getBrand(this.props.id, this.props.name, this.props.industry, this.props.slogan, this.props.logo, this.props.mainColor, this.props.supportingColor,this.props.images)
+      .then(res => this.setState({ brand: res.data }))
+      .catch(err => console.log(err));
+  }
 
   loadBrands = id => {
     API.getBrands(id)
@@ -41,32 +44,29 @@ class Brands extends Component {
 //       .catch(err => console.log(err));
 //   };
 
-  saveChanges = (id, body) => {
-        console.log(id)
-        API.updateBrand(id, body)
-          .then(res => this.props.history.push('/brands'))
-          .catch(err => console.log(err));
-      }
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
+  // saveChanges = (id, body) => {
+  //       console.log(id)
+  //       API.updateBrand(id, body)
+  //         .then(res => this.props.history.push('/brands'))
+  //         .catch(err => console.log(err));
+  //     }
+  handleTextChange = event => {
+    this.setState({ brand: event.target.value });
   };
+
 
   handleFormSubmit = id => {
     // event.preventDefault();
     //   API.updateBrand({
     API.updateBrand(id, 
         {
-        name: this.state.brandName,
-        industry: this.state.brandIndustry,
-        slogan: this.state.brandSlogan,
-        logo: this.state.brandLogo,
-        mainColor: this.state.brandMainColor,
-        supportingColor: this.state.brandSupportingColor,
-        images: this.state.brandImages
+        name: this.state.brand.name,
+        industry: this.state.brand.industry,
+        slogan: this.state.brand.slogan,
+        logo: this.state.brand.logo,
+        mainColor: this.state.brand.mainColor,
+        supportingColor: this.state.brand.supportingColor,
+        images: this.state.brand.images
         }
       )
       .then(res => this.loadBrands(), 
@@ -79,76 +79,76 @@ class Brands extends Component {
   render() {
     return (
       <div className="App">
-      <h2>Edit {this.props.brandName}</h2>
+      <h2>Edit {this.state.brand.name}</h2>
       <p>ID: {this.props.id}</p>
               <form>
                 <h4>Name</h4>
                 <TextArea
-                  value={this.state.brandName}
+                  value={this.state.brand.name}
+                  onChange={this.handleTextChange}
                   // value={this.props.brandName}
-                  onChange={this.handleInputChange}
                   name="name"
                   placeholder="Company Name (Required)"
                 />
                 <h4>Industry</h4>
                 <TextArea
-                  value={this.state.brandIndustry}
+                  value={this.state.brand.industry}
+                  onChange={this.handleTextChange}
                   // value={this.props.brandIndustry}
-                  onChange={this.handleInputChange}
                   name="industry"
                   placeholder="Industry (Required)"
                 />
                 <h4>Slogan</h4>
                 <TextArea
-                  value={this.state.brandSlogan}
+                  value={this.state.brand.slogan}
+                  onChange={this.handleTextChange}
                   // value={this.props.brandSlogan}
-                  onChange={this.handleInputChange}
                   name="slogan"
                   placeholder="Slogan (Optional)"
                 />
                 <h4>Logo</h4>
                 <TextArea
-                  value={this.state.brandLogo}
+                  value={this.state.brand.logo}
+                  onChange={this.handleTextChange}
                   // value={this.props.brandLogo}
-                  onChange={this.handleInputChange}
                   name="logo"
                   placeholder="Logo Image Link (Optional)"
                 />
                 <h4>Additional Image</h4>
                 <TextArea
-                  value={this.state.brandImages}
+                  value={this.state.brand.images}
+                  onChange={this.handleTextChange}
                   // value={this.props.brandImages}
-                  onChange={this.handleInputChange}
                   name="images"
                   placeholder="Additional Image Link (Optional)"
                 />
                 <h4>Main Color (HEX Code)</h4>
                 <TextArea
-                  value={this.state.brandMainColor}
+                  value={this.state.brand.mainColor}
+                  onChange={this.handleTextChange}
                   // value={this.props.brandMainColor}
-                  onChange={this.handleInputChange}
                   name="mainColor"
                   placeholder="Main Color HEX Code (Optional)"
                 />
                 <h4>Supporting Color (HEX Code)</h4>
                 <TextArea
-                  value={this.state.brandSupportingColor}
+                  value={this.state.brand.supportingColor}
+                  onChange={this.handleTextChange}
                   // value={this.props.brandSupportingColor}
-                  onChange={this.handleInputChange}
                   name="supportingColor"
                   placeholder="Supporting Color HEX Code (Optional)"
                 />
                 <button className="btn" value={this.props.id} 
                 // onClick={this.handleFormSubmit}
                 onClick={() => this.handleFormSubmit(
-                this.state.id, 
-                // this.state.brandName,
-                // this.state.brandIndustry,
-                // this.state.brandSlogan,
-                // this.state.brandLogo,
-                // this.state.brandImages,
-                // this.state.brandMainColor,
-                // this.state.brandSupportingColor
+                this.props.id, 
+                // this.state.brand.name,
+                // this.state.brand.industry,
+                // this.state.brand.slogan,
+                // this.state.brand.logo,
+                // this.state.brand.images,
+                // this.state.brand.mainColor,
+                // this.state.brand.supportingColor
                 )}
                 >Save Updates</button>
               </form>
